3de104173b63314f1de1ceea5e2a0067c1e7612a
FC4RICA <kantinan.fw@gmail.com>
2024-08-09 02:40:43 +0700

    Add CarRepository Dependency Injection

--- AirCoil-API/Program.cs
+++ AirCoil-API/Program.cs
@@ -16,6 +16,7 @@ builder.Services.AddControllers();
 builder.Services.AddTransient<Seed>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<ICarRepository, CarRepository>();
+builder.Services.AddScoped<IProvinceRepository, ProvinceRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
