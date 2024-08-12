8075d0dee8c9b714706662fa0a4edd954d1dd2e7
FC4RICA <kantinan.fw@gmail.com>
2024-08-13 02:24:31 +0700

    Add ServiceCenter Repository

---
+++ AirCoil-API/Interface/IServiceCenterRepository.cs
@@ -0,0 +1,15 @@
+﻿using AirCoil_API.Models;
+
+namespace AirCoil_API.Interface
+{
+    public interface IServiceCenterRepository
+    {
+        ICollection<ServiceCenter> GetServiceCenters();
+        ServiceCenter GetServiceCenter(int id);
+        bool CreateServiceCenter(ServiceCenter serviceCenter);
+        bool UpdateServiceCenter(ServiceCenter serviceCenter);
+        bool DeleteServiceCenter(ServiceCenter serviceCenter);
+        bool ServiceCenterExists(int id);
+        bool Save();
+    }
+}
---
+++ AirCoil-API/Repository/ServiceCenterReposotory.cs
@@ -0,0 +1,55 @@
+﻿using AirCoil_API.Data;
+using AirCoil_API.Interface;
+using AirCoil_API.Models;
+
+namespace AirCoil_API.Repository
+{
+    public class ServiceCenterReposotory : IServiceCenterRepository
+    { 
+        private readonly DataContext _context;
+
+        public ServiceCenterReposotory(DataContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<ServiceCenter> GetServiceCenters()
+        {
+            return _context.ServiceCenters.OrderBy(s => s.Id).ToList();
+        }
+
+        public ServiceCenter GetServiceCenter(int id)
+        {
+            return _context.ServiceCenters.Where(s => s.Id == id).FirstOrDefault();
+        }
+
+        public bool CreateServiceCenter(ServiceCenter serviceCenter)
+        {
+            _context.ServiceCenters.Add(serviceCenter);
+            return Save();
+        }
+
+        public bool UpdateServiceCenter(ServiceCenter serviceCenter)
+        {
+            _context.ServiceCenters.Update(serviceCenter);
+            return Save();
+        }
+
+        public bool DeleteServiceCenter(ServiceCenter serviceCenter)
+        {
+            _context.ServiceCenters.Remove(serviceCenter);
+            return Save();
+        }
+
+        public bool ServiceCenterExists(int id)
+        {
+            return _context.ServiceCenters.Any(s => s.Id == id);
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0;
+        }
+    }
+}
